997239e167a4735a0743b239a3f7cf0f93fe844a
Matt Wood <mza@macbook.config>
2008-12-18 21:17:49 +0000

    Added capistrano deployment. Added production details.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,35 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "maverick"
+set :scm, "git"
+set :repository,  "git@github.com:mza/#{application}.git"
+set :branch, "master"
+set :user, "root"
+set :deploy_to, "/var/rails/#{application}"
+set :scm_verbose, true
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+ssh_options[:forward_agent] = true
+ssh_options[:keys] = ["#{ENV['HOME']}/.ec2/id_rsa-gsg-keypair"]
+default_run_options[:pty] = true
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+role :app, "injectify.com"
+role :web, "injectify.com"
+role :db,  "injectify.com", :primary => true
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+after :deploy, "set_ownership"
+
+namespace :deploy do
+  
+  desc "Correctly sets ownership of environment.rb"  
+  task :set_ownership do
+    run "chown www-data:www-data #{current_path}/config/environment.rb"
+  end
+  
+  desc "Restarting Passenger with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} task is unnecessary with Passenger"
+    task t, :roles => :app do ; end
+  end
+end
